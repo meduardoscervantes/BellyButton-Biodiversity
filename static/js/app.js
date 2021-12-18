37b898f0fb4210f292303cbbb1ca09acbf619e5b
Manuel Eduardo Sotelo Cervantes <meduardoscervantes@gmail.com>
2021-12-18 13:09:03 -0600

    Wrote the code create the horizontal bar chart

--- static/js/app.js
+++ static/js/app.js
@@ -1,13 +1,13 @@
 /**
  * read the json file using d3
  */
- let data = d3.json("data/samples.json").then((rawData) => {
+let data = d3.json("data/samples.json").then((rawData) => {
     /**
      * create var for name and load that data into the <select>
      */
     let names = rawData.names;
-    function loadNames(){
-        for(let i = 0; i<names.length; i++){
+    function loadNames() {
+        for (let i = 0; i < names.length; i++) {
             let opt = document.createElement('option');
             opt.value = i;
             opt.innerHTML = names[i];
@@ -19,10 +19,41 @@
     /**
      * when a name is selected update the page with the new data
      */
-     document.getElementById('selDataset').onchange = function optionChanged(){
+    document.getElementById('selDataset').onchange = function optionChanged() {
+        /**
+         * locate the selected index
+         * */
         let index = document.getElementById('selDataset').value;
-        console.log(index);
-        let y = document.getElementById('selDataset')[x].innerHTML;
-        console.log(y);
-     };
+
+        // generate key-value pair from the metadata using the index and add it
+        //     to the associated metadata div
+        document.getElementById('sample-metadata').innerHTML = "";
+        for (const [key, value] of Object.entries(rawData.metadata[index])) {
+            let str = document.createElement('h5')
+            str.innerHTML = `${key}: ${value}`
+            document.getElementById('sample-metadata').appendChild(str);
+        };
+
+        /**
+         * find the top ten bacterias in the samples and create the bar chart
+         */
+        let sample = rawData.samples[index];
+        let xAxis = [];
+        let yAxis = [];
+        let hoverText = [];
+        for (let i = 0; i < 10; i++) {
+            yAxis.push("OTU " + sample.otu_ids[i])
+            xAxis.push(sample.sample_values[i]);
+            hoverText.push(sample.otu_labels[i]);
+        };
+        // create the horizontal bar chart
+        let hBar = [{
+            type: 'bar',
+            x: xAxis.reverse(),
+            y: yAxis.reverse(),
+            orientation: 'h',
+            text: hoverText
+        }];
+        Plotly.newPlot("bar", hBar);
+    };
 });
