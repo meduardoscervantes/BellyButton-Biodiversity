fdeb699d225dd11b9491bae3d8427a28d8b646b2
Manuel Eduardo Sotelo Cervantes <meduardoscervantes@gmail.com>
2021-12-18 14:01:06 -0600

    updated code structuire

--- static/js/app.js
+++ static/js/app.js
@@ -5,8 +5,7 @@ let data = d3.json("data/samples.json").then((rawData) => {
     /**
      * create var for name and load that data into the <select>
      */
-    let names = rawData.names;
-    function loadNames() {
+    function loadNames(names) {
         for (let i = 0; i < names.length; i++) {
             let opt = document.createElement('option');
             opt.value = i;
@@ -14,26 +13,55 @@ let data = d3.json("data/samples.json").then((rawData) => {
             document.getElementById('selDataset').appendChild(opt);
         };
     };
-    loadNames();
+
+    /**
+     * initial load ins for the page
+     */
+    // load the names into the page.
+    loadNames(rawData.names);
+    //load meta-data
+    loadMetaData();
+    //create the hbar chart
+    createBar(document.getElementById('selDataset').value);
+    //load bubble chart
+    createBubble(document.getElementById('selDataset').value);
 
     /**
      * when a name is selected update the page with the new data
      */
     document.getElementById('selDataset').onchange = function optionChanged() {
+        // re-load the meta-data
+        loadMetaData();
+        // re-load the bar chart
+        createBar(document.getElementById('selDataset').value);
+
+        // re-load the bubble chart
+        createBubble(document.getElementById('selDataset').value);
+    };
+
+    /**
+     * Update the meta-data
+     */
+    function loadMetaData(){
         /**
          * locate the selected index
-         * */
-        let index = document.getElementById('selDataset').value;
+        * */
+         let index = document.getElementById('selDataset').value;
 
-        // generate key-value pair from the metadata using the index and add it
-        //     to the associated metadata div
-        document.getElementById('sample-metadata').innerHTML = "";
-        for (const [key, value] of Object.entries(rawData.metadata[index])) {
-            let str = document.createElement('h5')
-            str.innerHTML = `${key}: ${value}`
-            document.getElementById('sample-metadata').appendChild(str);
-        };
+         // generate key-value pair from the metadata using the index and add it
+         //     to the associated metadata div
+         document.getElementById('sample-metadata').innerHTML = "";
+         for (const [key, value] of Object.entries(rawData.metadata[index])) {
+             let str = document.createElement('h5')
+             str.innerHTML = `${key}: ${value}`
+             document.getElementById('sample-metadata').appendChild(str);
+         };
+    };
 
+    /**
+     * Generate hotizontal bar chart
+     */
+    function createBar(index) {
         /**
          * find the top ten bacterias in the samples and create the bar chart
          */
@@ -47,18 +75,31 @@ let data = d3.json("data/samples.json").then((rawData) => {
         // create the horizontal bar chart
         let hBar = [{
             type: 'bar',
-            x: xAxis.slice(0,9).reverse(),
-            y: yAxis.slice(0,9).reverse(),
+            x: xAxis.slice(0, 9).reverse(),
+            y: yAxis.slice(0, 9).reverse(),
             orientation: 'h',
-            text: hoverText.slice(0,9).reverse()
+            text: hoverText.slice(0, 9).reverse()
         }];
-        Plotly.newPlot("bar", hBar);
-
-        let testX = sample.sample_values;
-        console.log(testX);
+        let layout = {
+            showlegend: false
+        };
+        Plotly.newPlot("bar", hBar, layout);
     };
-    
+
     /**
     * create the bubble chart
     */
+    function createBubble(index){
+        /**
+        * create the bubble chart
+        */
+         let sample = rawData.samples[index];
+         let bubble = {
+            
+         };
+         let layout = {
+            showlegend: false
+        };
+         Plotly.newPlot('bubble', bubble, layout);
+    };
 });
